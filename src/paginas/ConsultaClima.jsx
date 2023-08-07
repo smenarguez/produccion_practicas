7bdb4018c7475ee2745ffce99ef4d09262d91249
Salva <smenarguez@gmail.com>
2023-08-07 10:42:40 +0200

    Componente ConsultaClima

--- src/paginas/ConsultaClima.jsx
+++ src/paginas/ConsultaClima.jsx
@@ -1,8 +1,157 @@
-import React from "react";
+import React, {useState} from "react";
+import {Box, Container, TextField, Typography}  from '@mui/material/';
+import {LoadingButton} from '@mui/lab';
+
+const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=`;
 
 const ConsultaClima = () => {
+    const [city, setCity] = useState('Madrid');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState({
+        error: false,
+        message: " "
+    });
+
+    const [weather, setWeather] = useState({
+        city: 'Madrid',
+        country: 'Spain',
+        temperature: 22,
+        condition: '',
+        temp_c: '',
+        conditionText: 'Party Cloudy'
+    });
+
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        
+        setLoading(true);
+
+        setError({
+            error: false,
+            message: error.message
+        })
+
+        try {
+            if(!city.trim()) throw { message: 'El campo ciudad es obligatorio'};
+            const response = await fetch(`${API_WEATHER}${city}`);
+            const data = await response.json();
+
+            if(data.error) throw { message: data.error.message};
+
+            setWeather({
+                city: data.location.name,
+                country: data.location.country,
+                temperature: data.current.temp_c,
+                condition: data.current.condition.icon,
+                conditionText: data.current.condition.text
+            })
+           
+        }catch(error){
+            setError({
+                error: true,
+                message: error.message
+            })
+        } finally{
+            setLoading(false);
+        }
+    }
+
     return ( 
-        <h1>Consulta clima</h1>
+        <>
+        <Container sx={{border: '3', boxShadow: 3, pb: 2, mt: 4}}>
+            
+            <Box sx={{mb: 3}}>
+                <Typography
+                    variant="h5"
+                    component='h5'
+                    sx={{ml:9}}
+                >
+                    Consulta de clima
+                </Typography>
+
+                <Box
+                    sx={{display: 'grid', gap: 2, align:'center', width: 400, ml: 50}}
+                    component='form'
+                    autoComplete="off"
+                    onSubmit={onSubmit}
+                >
+                    <TextField
+                        id="city"
+                        label='Ciudad'
+                        variant="outlined"
+                        size="small"
+                        required
+                        fullWidth
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                        error={error.error}
+                        helperText={error.message}
+                    />
+
+                    <LoadingButton
+                        type='submit'
+                        variant='contained'
+                        loading={loading}
+                        loadingIndicator='Cargando...'
+                    >
+                        Buscar
+                    </LoadingButton>
+            </Box>
+
+            { weather.city && (
+                <Box
+                    sx={{
+                        mt: 2,
+                        display: 'grid',
+                        gap: 2,
+                        textAlign: 'center'
+                    }}
+                >
+                    <Typography variant="h4" component='h2' >
+                        {weather.city}, {weather.country}
+                    </Typography>
+
+                    <Box
+                        component = 'img'
+                        alt={weather.conditionText}
+                        src={weather.icon}
+                        sx={{margin: '0 auto'}}
+                    />
+
+                    <Typography variant="h5" component='h3' >
+                        {weather.temperature} ºC
+                    </Typography>
+
+                    <Typography variant="h6" component='h4' >
+                        {weather.conditionText}
+                    </Typography>
+                </Box>
+            )
+
+            }
+
+            <Typography
+                textAlign='center'
+                sx={{mt: 2, fontSize: '10px'}}
+            >
+                Powered by: {" "}
+                <a
+                    href="https://www.weatherapi.com/"
+                    title="Weather API"
+                >
+                    WeatherAPI.com
+                </a>
+            </Typography>
+            </Box>
+        </Container>
+
+        <Container
+            maxWidth='xs'
+            sx={{mt: 2}}
+        >
+            
+        </Container>
+        </>
      );
 }
  
